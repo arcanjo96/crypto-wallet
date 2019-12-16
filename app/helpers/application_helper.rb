3c3c173b9122feef2166cda854ffeda33167f075
arcanjo96 <lucasarcanjo.ld@gmail.com>
2019-12-16 20:29:51 -0300

    Added I18n.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def date_format(date)
     date.strftime("%d de %B de %Y")
   end
+
+  def locale(locale)
+    I18n.locale == :en ? "Estados Unidos" : "Português Brasil"
+  end
 end
